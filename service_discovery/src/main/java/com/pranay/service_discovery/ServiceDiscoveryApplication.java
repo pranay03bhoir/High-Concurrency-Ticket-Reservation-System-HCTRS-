ddc8f8fb54865b79e59d8216db2924b90709de5d
pranaybhoir <pranaytb777@gmail.com>
2026-09-07 19:20:41 +0530

    Add configuration files and dependencies for service discovery, config server, and inventory service

--- service_discovery/src/main/java/com/pranay/service_discovery/ServiceDiscoveryApplication.java
+++ service_discovery/src/main/java/com/pranay/service_discovery/ServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.pranay.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
